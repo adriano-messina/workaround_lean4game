8e11dd3bff9df56f88da51676e9a199e5db56027
Adriano Messina <185371389+adriano-messina@users.noreply.github.com>
2025-02-06 00:21:26 +0100

    correct way of calling the functions, tested

--- installation.sh
+++ installation.sh
@@ -8,18 +8,16 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sudo adduser lean4game --gecos "" --disabled-password
 # strongly recommendent, since lean4 / lean4game has poor installer
 
-# change only the owner of the hidden files to lean4game
-sudo chown -cR lean4game $SCRIPT_DIR/.installation_without_sudo.sh
-sudo chown -cR lean4game $SCRIPT_DIR/.gameskeleton_without_sudo.sh
-sudo chown -cR lean4game $SCRIPT_DIR/.run_without_sudo.sh
-
-
 # Lean4, Lean4game needs some dependecies that are sadly not explicitly listet in their documentation
 # so there are they:
 sudo apt install git curl cmake make cpp g++
 # maybe not everything is needed, but since it worked on a fresh Ubuntu 22.04 LTS install, it should be fine
 # if you had installed those you don't need root privileges
 
+# create a copy of all files for the lean4game user in his homedirectory
+cd ~lean4game
+sudo -u lean4game git clone https://github.com/adriano-messina/workaround_lean4game.git
+cd $SCRIPT_DIR
 
 # execute the part that not need sudo, luckyly
-sudo -u lean4game sh $SCRIPT_DIR/.installation_without_sudo.sh
+sudo -u lean4game sh ~lean4game/workaround_lean4game/.installation_without_sudo.sh
